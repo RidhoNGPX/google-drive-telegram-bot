ce545c3700badb3eea2748bc82047ab5db82b5d3
Ridho <89362181+RidhoNGPX@users.noreply.github.com>
2022-12-09 00:11:14 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -20,7 +20,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 if __name__ == "__main__":
     if not os.path.isdir(DOWNLOAD_DIRECTORY):
         os.makedirs(DOWNLOAD_DIRECTORY)
-    )
+
     app = Client(
         "G-DriveBot",
         bot_token=BOT_TOKEN,
